abc7775b0786f5faa2d8d1d29785fcfa63538a3a
tujietg <tujietg@gmail.com>
2018-09-12 11:29:57 +0800

    add other method (No28.java)

--- String/No28.java
+++ String/No28.java
@@ -9,3 +9,18 @@ class Solution {
         return -1;
     }
 }
+public int strStr(String haystack, String needle) {
+        int hayLen = haystack.length() , needLen = needle.length();
+        if(hayLen < needLen){
+            return -1;
+        }else if(needLen == 0){
+            return 0;
+        }
+        int dum = hayLen - needLen;
+        for(int i = 0; i <= dum ; i++){
+            if((haystack.substring(i,i+needLen)).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
