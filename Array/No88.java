032f047000b0e868b9a957a3fbd4af3a0ab321e4
tujietg <tujietg@gmail.com>
2018-09-16 21:42:16 +0800

    add a nwe method

--- Array/No88.java
+++ Array/No88.java
@@ -11,3 +11,18 @@ public class No88 {
 		Arrays.sort(nums1);
 	}
 }
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m -1,j = n - 1, k = m + n - 1;
+        while(i > -1 && j > -1){
+            if(nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }else{
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while(j > -1){
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
