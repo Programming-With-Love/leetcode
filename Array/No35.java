709588a665508df59c25bfeed3013272498ba3c7
tujietg <tujietg@gmail.com>
2018-09-17 16:08:22 +0800

    nodified

--- Array/No35.java
+++ Array/No35.java
@@ -11,3 +11,20 @@ public class No35 {
 		return k;
 	}
 }
+public int searchInsert(int[] nums, int target) {
+        for(int i = 0;i < nums.length; i++){
+            if(nums[i] == target){
+                return i;
+            }
+            if(nums[0] > target){
+                return 0;
+            }
+            if(nums[nums.length - 1] < target){
+                return nums.length;
+            }
+            if(nums[i] < target && target < nums[i+1]){
+                return i + 1;
+            }
+        }
+        return -1;
+    }
--- Array/No53.java
+++ Array/No53.java
@@ -30,4 +30,25 @@ public class No53 {
 		}
 		return max;
 	}
+public int maxSubArray(int[] nums) {
+if(nums.length == 0){
+            return 0;
+        }
+        int sum = nums[0];
+        int max = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(sum > 0){
+               sum += nums[i];
+            }else{
+               sum = nums[i];
+            }
+            if(max < sum){
+             
+	        max = sum;
+            }
+        }
+        return max;
+    }
 }
+
+
