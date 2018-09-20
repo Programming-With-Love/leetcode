65625f015ea2f3a860fb841080a4fdaea709f863
tujietg <tujietg@gmail.com>
2018-09-20 19:47:18 +0800

    No136

---
+++ leetcode/math/No136.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums.length == 1){
+            return nums[0];
+        }
+        Arrays.sort(nums);
+        for(int i = 0;i < nums.length;i++){
+            if(i == 0){
+                if(nums[0] != nums[1]){
+                    return nums[0];
+                }
+            }else if(i == nums.length - 1){
+                if(nums[nums.length - 2] != nums[nums.length - 1]){
+                    return nums[nums.length - 1];
+                }
+            }else if(nums[i] != nums[i-1] && nums[i] != nums[i+1]){
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+}
