2efb1109f8a157ec9824174f80e8d5d8d824e4d3
tujietg <tujietg@gmail.com>
2018-09-24 17:05:04 +0800

    No561

---
+++ leetcode/Array/No561.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        int result = 0;
+        Arrays.sort(nums);
+        for(int i = 0;i < nums.length;i += 2){
+            result += nums[i];
+        }
+        return result;
+    }
+}
