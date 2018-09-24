899bb5d5ba53111a3b1e48c2928507232a1e0106
tujietg <tujietg@gmail.com>
2018-09-24 16:58:03 +0800

    No908

---
+++ leetcode/Array/908.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int smallestRangeI(int[] A, int K) {
+        if(A.length == 0 || A.length == 1){
+            return 0;
+        }
+        //[2,7,2] [2,2,7] K = 1
+        //[1,3,6] 3
+        int len = A.length;
+        Arrays.sort(A);
+        if(A[len - 1] - A[0] <= 2*K){
+            return 0;
+        }else{
+            return A[len-1] - 2*K - A[0];
+        }
+    }
+}
