5821f2c8f5dbcfb3df15de473327a56b0c496208
tujietg <tujietg@gmail.com>
2019-01-17 10:30:53 +0800

    add No172

---
+++ leetcode/math/No172.java
@@ -0,0 +1,11 @@
+
+public class No172 {
+    public int trailingZeroes(int n) {
+        int res = 0;
+        while(n != 0){
+            res+=n/5;
+            n=n/5;
+        }
+        return res;
+    }
+}
