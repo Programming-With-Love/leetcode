ab6416767c7e452df6f0a8405e32e94b01cb9f0c
tujietg <tujietg@gmail.com>
2019-01-24 10:43:46 +0800

    add No205

---
+++ leetcode/String/No205.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() <= 1 || s == null){
+            return true;
+        }
+        Map<Character,Character> hashMap = new HashMap<Character,Character>();
+        for(int i = 0;i < s.length();i++){    
+             char a = s.charAt(i);
+            char b = t.charAt(i);
+            if(hashMap.containsKey(a)){
+                if(hashMap.get(a).equals(b)){
+                    continue;
+                }else{
+                    return false;
+                }
+            }else{
+                if(!hashMap.containsValue(b)){
+                    hashMap.put(a,b);
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
