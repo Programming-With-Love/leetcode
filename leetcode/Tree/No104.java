022f436f04bba45da7ecec7b7f46e5f158f02b5e
tujietg <tujietg@gmail.com>
2019-06-03 19:06:42 +0800

    整理代码

--- leetcode/Tree/No104.java
+++ leetcode/Tree/No104.java
@@ -1,17 +1,25 @@
+package Algorithm.leetcode.Tree;
+
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
  */
-class Solution {
-    public int maxDepth(TreeNode root) {
-        if(root == null){
-            return 0;
-        }
-        return 1 + Math.max(maxDepth(root.left),(root.right));
-    }
+
+class TreeNodeInt {
+	int val;
+	TreeNodeInt left;
+	TreeNodeInt right;
+
+	TreeNodeInt(int x) {
+		val = x;
+	}
+}
+
+public class No104 {
+	public int maxDepth(TreeNodeInt root) {
+		if (root == null) {
+			return 0;
+		}
+		return 1 + Math.max(maxDepth(root.left), (root.right));
+	}
 }
