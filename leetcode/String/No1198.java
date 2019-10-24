57f98c7c97702767f981423299bcf1d501183145
tujietg <tujietg@gmail.com>
2019-10-24 18:49:55 +0800

    增加1198

---
+++ leetcode/String/No1198.java
@@ -0,0 +1,38 @@
+package Algorithm.leetcode.String;
+
+/**
+ * 
+ * Created by tujietg on Oct 24, 2019
+ */
+public class No1198 {
+	public int maxNumberOfBalloons(String text) {
+		int a = 0;
+		int b = 0;
+		int o = 0;
+		int l = 0;
+		int n = 0;
+		for (int i = 0; i < text.length(); i++) {
+			char item = text.charAt(i);
+			if (item == 'a') {
+				a = a + 1;
+			} else if (item == 'b') {
+				b++;
+			} else if (item == 'l') {
+				l++;
+			} else if (item == 'o') {
+				o++;
+			} else if (item == 'n') {
+				n++;
+			} else {
+			}
+		}
+		l = l / 2;
+		o = o / 2;
+		int minText = 0;
+		minText = Math.min(a, b);
+		minText = Math.min(minText, l);
+		minText = Math.min(minText, o);
+		minText = Math.min(minText, n);
+		return minText;
+	}
+}
