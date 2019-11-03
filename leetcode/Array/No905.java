e6658a5b61b73e159c0148c97e735f9defe2f175
tujietg <tujietg@gmail.com>
2019-11-03 18:38:00 +0800

    增加No905

---
+++ leetcode/Array/No905.java
@@ -0,0 +1,21 @@
+package Algorithm.leetcode.Array;
+
+/**
+ * 
+ * Created by tujietg on Nov 3, 2019
+ */
+public class No905 {
+	public int[] sortArrayByParity(int[] A) {
+		int a = 0;
+		int b = A.length - 1;
+		int[] end = new int[A.length];
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] % 2 == 0) {
+				end[a++] = A[i];
+			} else {
+				end[b--] = A[i];
+			}
+		}
+		return end;
+	}
+}
