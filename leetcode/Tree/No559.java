e88f0fa6690d57a146e14441ac887ca686ee35cc
tujietg <tujietg@gmail.com>
2020-01-22 18:38:13 +0800

    增加No559

---
+++ leetcode/Tree/No559.java
@@ -0,0 +1,23 @@
+package Algorithm.leetcode.Tree;
+
+/**
+ * No559
+ * 
+ * Created by tujietg on Jan 22, 2020
+ */
+public class No559 {
+	public int maxDepth(Node root) {
+		if (root == null) {
+			return 0;
+		}
+		int end = 0;
+		for (Node node : root.children) {
+			int value = maxDepth(node);
+
+			if (value > end) {
+				end = value;
+			}
+		}
+		return end + 1;
+	}
+}
