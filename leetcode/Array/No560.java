5285f48a00595a4cd9eb14d01c81218ada146a2d
tujietg <tujietg@gmail.com>
2020-05-15 11:00:27 +0800

    增加No560

---
+++ leetcode/Array/No560.java
@@ -0,0 +1,19 @@
+/**
+ * @author tujietg
+ * @date 5/15/20 10:59 AM
+ */
+public class No560 {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            int sum = 0;
+            for (int end = start; end >= 0; end--) {
+                sum += nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
