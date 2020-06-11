582913027ef681e2b9706dd3b3bf8f9fc7a46571
tujietg <tujietg@gmail.com>
2020-06-11 09:30:07 +0800

    增加Nom35

---
+++ LinkedList/Nom35.java
@@ -0,0 +1,39 @@
+package LinkedList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author tujietg
+ * @date 6/11/20 9:11 AM
+ */
+public class Nom35 {
+
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+        Node p = head;
+        while (p != null) {
+            map.put(p, new Node(p.val));
+            p = p.next;
+        }
+        p = head;
+        while (p != null) {
+            map.get(p).next = map.get(p.next);
+            map.get(p).random = map.get(p.random);
+            p = p.next;
+        }
+        return map.get(head);
+    }
+
+    private class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
